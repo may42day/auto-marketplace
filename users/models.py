394d519073e92e5523b00a956294ebcc9b803a53
Vlad <vladh17dmv@mail.ru>
2022-09-29 22:56:26 +0300

    feat: add new user's functions

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    CUSTOMER = 'C'
+    SELLER = 'S'
+    USER_TYPES = [
+        (CUSTOMER, 'Покупатель'),
+        (SELLER, 'Продавец')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=1, choices=USER_TYPES, default='C')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UserSignUpForm, ProfileForm
 
-# Create your views here.
+def start_page(request):
+    return render(request, 'users/start_page.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        user_form = UserSignUpForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile_form = ProfileForm(request.POST, instance=user)
+            profile_form.save()
+
+
+            user.set_password(user_form.cleaned_data['password'])
+            user.is_active = True
+            user.save()
+
+
+
+            return HttpResponseRedirect('/')
+    else:
+        user_form = UserSignUpForm()
+        profile_form = ProfileForm()
+    return render(request, 'users/sign_up.html', context={
+        'user_form':user_form,
+        'profile_form':profile_form,
+    })
