15fff1dec0aa6e9741f17787c31a2672be548639
Vlad <vladh17dmv@mail.ru>
2022-09-30 21:52:31 +0300

    feat: add app goods

--- users/views.py
+++ users/views.py
@@ -13,15 +13,13 @@ def sign_up(request):
         profile_form = ProfileForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
-            profile_form = ProfileForm(request.POST, instance=user)
-            profile_form.save()
-
-
             user.set_password(user_form.cleaned_data['password'])
             user.is_active = True
+            user.profile.user_type = profile_form.cleaned_data['user_type']
             user.save()
 
-
+            # profile_form = ProfileForm(request.POST, instance=user)
+            # profile_form.save()
 
             return HttpResponseRedirect('/')
     else:
