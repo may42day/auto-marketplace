1a54d3208140b92a91e65cfa06318d8c63ed56f8
Vlad <vladh17dmv@mail.ru>
2022-09-30 21:52:31 +0300

    feat: add app goods

--- users/models.py
+++ users/models.py
@@ -4,20 +4,26 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    CUSTOMER = 'C'
-    SELLER = 'S'
+    CUSTOMER = 'Customer'
+    SELLER = 'Seller'
     USER_TYPES = [
-        (CUSTOMER, 'Покупатель'),
-        (SELLER, 'Продавец')
+        (CUSTOMER, 'Customer'),
+        (SELLER, 'Seller')
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=1, choices=USER_TYPES, default='C')
+    user_type = models.CharField(max_length=8, choices=USER_TYPES, default='Customer')
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+    def __str__(self):
+        if self.user_type == self.CUSTOMER:
+            return 'Customer'
+        else:
+            return 'Seller'
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
