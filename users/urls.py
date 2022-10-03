76bd346d2ef8c35585d6c4f037d6c16e0132771f
Vlad <vladh17dmv@mail.ru>
2022-10-03 22:30:59 +0300

    update app 'goods'

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from .views import start_page, sign_up
 
 urlpatterns = [
-    path('', start_page),
+    path('', start_page, name='start-page'),
     path('sign_up', sign_up, name='sign_up')
 ]
 
