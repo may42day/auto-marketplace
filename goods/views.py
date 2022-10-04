8839a6178ccff903a461db1bfe17c80fd10dc634
Vlad <vladh17dmv@mail.ru>
2022-10-04 23:43:39 +0300

    feat: new models(product, category)

--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 
 def create_product_card(request):
     return render(request, 'goods/new_product_card.html')
 
 def market(request):
-    return render(request, 'goods/market.html')
+    categories = Category.objects.all()
+    return render(request, 'goods/market.html', context={
+        'categories':categories
+    })
+
+
+def category(request, slug_category:str):
+    category = get_object_or_404(Category, slug=slug_category)
+    products = Product.objects.filter(category=category.id)
+    return render(request, 'goods/category.html', context={
+        'category':category,
+        'products':products,
+    })
