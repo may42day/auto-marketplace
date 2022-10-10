63585959a56038398c2d46ef27fbf5e693eb1b56
Vlad <vladh17dmv@mail.ru>
2022-10-10 17:37:55 +0300

    feat: expand produts app

--- goods/views.py
+++ goods/views.py
@@ -6,12 +6,10 @@ from .forms import NewCardForm
 def create_product_card(request):
     if request.method == 'POST':
         card_form = NewCardForm(request.POST)
-        if NewCardForm.is_valid():
-            # user = user_form.save()
-            # user.set_password(user_form.cleaned_data['password'])
-            # user.is_active = True
-            # user.profile.user_type = profile_form.cleaned_data['user_type']
-            # user.save()
+        if card_form.is_valid():
+            product = card_form.save(commit=False)
+            product.owner = request.user
+            product.save()
             return HttpResponseRedirect('/')
     else:
         card_form = NewCardForm()
@@ -19,6 +17,11 @@ def create_product_card(request):
         'card_form':card_form,
     })
 
+def product_card(request, slug_category:str, product_id:int):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'goods/product_card.html', context={
+        'product':product,
+    })
 def market(request):
     categories = Category.objects.all()
     return render(request, 'goods/market.html', context={
@@ -32,4 +35,10 @@ def category(request, slug_category:str):
     return render(request, 'goods/category.html', context={
         'category':category,
         'products':products,
+    })
+
+def products_on_moderation(request):
+    products = Product.objects.filter(on_moderation=True)
+    return render(request, 'goods/ad_moderation.html', context={
+        'products': products,
     })
