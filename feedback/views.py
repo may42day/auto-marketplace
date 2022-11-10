b04c3159b82e9bf6a4412299756063064899a667
Vlad <vladh17dmv@mail.ru>
2022-11-10 23:46:27 +0300

    refactor feedback and product models

--- feedback/views.py
+++ feedback/views.py
@@ -16,10 +16,7 @@ def add_feedback(request, product_id):
     product = Product.objects.get(pk=product_id)
     feedback.product = product
     feedback.save()
-    avg_rating = round(Feedback.objects.filter(product=product).aggregate(Avg('rating'))['rating__avg'],1)
-    product.average_rating = avg_rating
-    url = product.get_absolute_url()
-    product.save()
+    url = feedback.product.get_absolute_url()
     return HttpResponseRedirect(f'{url}')
 
 
@@ -27,13 +24,15 @@ def add_feedback(request, product_id):
 @login_required
 def remove_feedback(request, feedback_id):
     feedback = Feedback.objects.get(pk=feedback_id)
-    product = feedback.product
+    # product = feedback.product
     url = feedback.product.get_absolute_url()
+    feedback.calculate_average_rating(delete=True, exclude_pk=feedback.pk)
     if request.user == feedback.user:
         feedback.delete()
 
-    avg_rating = round(Feedback.objects.filter(product=product).aggregate(Avg('rating'))['rating__avg'], 1)
-    product.average_rating = avg_rating
-    product.save()
+
+    # avg_rating = round(Feedback.objects.filter(product=product).aggregate(Avg('rating'))['rating__avg'], 1)
+    # product.average_rating = avg_rating
+    # product.save()
     return HttpResponseRedirect(f'{url}')
 
